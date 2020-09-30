nuxeo/nuxeo-explorer

commit 50a7073f3ce3c10307f76d345115ca05c768d721

NXP-29624: remove duplicate retrieval of boolean props on update

modules/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/repository/RepositoryDistributionSnapshot.java
@@ -533,8 +533,6 @@ public class RepositoryDistributionSnapshot extends BaseNuxeoArtifactDocAdapter 
                     && StringUtils.isNotBlank((String) props.get(PROP_NAME))) {
                 props.put(TITLE_PROPERTY_PATH, props.get(PROP_NAME));
             }
-            List.of(PROP_LATEST_LTS, PROP_LATEST_FT, PROP_HIDE)
-                .forEach(p -> props.put(p, Boolean.toString(formFields.containsKey(p))));
         }
         return props;
     }

commit 1a2d9a0e94e6844b31c1605f81688177dbe8a0bd

NXP-29624: fix checking checkboxes when updating distribution.
Regression introduced by 50a7073f3ce3c10307f76d345115ca05c768d721
Unchecking the box still worked alright (as tested on ftests)

modules/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/repository/RepositoryDistributionSnapshot.java
@@ -535,14 +535,16 @@ public class RepositoryDistributionSnapshot extends BaseNuxeoArtifactDocAdapter 
     public static Map<String, Serializable> getUpdateProperties(Map<String, String[]> formFields) {
         Map<String, Serializable> props = new HashMap<>();
         if (formFields != null) {
-            Stream.of(TITLE_PROPERTY_PATH, PROP_NAME, PROP_VERSION, PROP_KEY, PROP_LATEST_LTS, PROP_LATEST_FT,
-                    PROP_HIDE, PROP_RELEASED, PROP_ALIASES)
+            Stream.of(TITLE_PROPERTY_PATH, PROP_NAME, PROP_VERSION, PROP_KEY, PROP_RELEASED, PROP_ALIASES)
                   .filter(formFields::containsKey)
                   .forEach(p -> props.put(p, formFields.get(p)[0]));
             if (StringUtils.isBlank((String) props.get(TITLE_PROPERTY_PATH))
                     && StringUtils.isNotBlank((String) props.get(PROP_NAME))) {
                 props.put(TITLE_PROPERTY_PATH, props.get(PROP_NAME));
             }
+            // checkboxes will fill the request (with value "on"), only when checked
+            List.of(PROP_LATEST_LTS, PROP_LATEST_FT, PROP_HIDE)
+                .forEach(p -> props.put(p, Boolean.toString(formFields.containsKey(p))));
         }
         return props;
     }