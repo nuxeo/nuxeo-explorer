0fc253d4fd6a0c10ed1d15fc7867c7ed29670d65
Anahide Tchertchian <atchertchian@nuxeo.com>
2020-10-01 17:03:44 +0200

    NXP-28838: remove useless table of contents on distrib index page
    Reverting change made with 3bb41e81eeaeedda20dff206101cc282969667f4, when adding exports

--- ftests/explorer-ftests-base/src/main/java/org/nuxeo/functionaltests/explorer/pages/DistributionHomePage.java
+++ ftests/explorer-ftests-base/src/main/java/org/nuxeo/functionaltests/explorer/pages/DistributionHomePage.java
@@ -69,10 +69,6 @@ public class DistributionHomePage extends AbstractExplorerPage {
     public WebElement packages;
 
     @Required
-    @FindBy(xpath = "//div[@id='tocDiv']")
-    public WebElement toc;
-
-    @Required
     @FindBy(xpath = "//ul[@class='exports']")
     public WebElement exports;
 
@@ -84,9 +80,6 @@ public class DistributionHomePage extends AbstractExplorerPage {
     public void check() {
         checkTitle("Nuxeo Platform Explorer");
         checkHeader(null);
-        assertEquals("Browse\n" //
-                + "Exports", //
-                toc.getText());
         // check export links presence but do not click (too costly)
         exports.findElement(By.linkText("Json Export"));
         exports.findElement(By.linkText("Json Graph"));
