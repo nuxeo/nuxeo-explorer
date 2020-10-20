d111fc1fa5eb752049ee355a4446c58472623603
Anahide Tchertchian <atchertchian@nuxeo.com>
2020-10-20 12:48:01 +0200

    NXP-29785: allow switching repository queries to ES

--- modules/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/search/ArtifactSearcherImpl.java
+++ modules/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/search/ArtifactSearcherImpl.java
@@ -51,8 +51,6 @@ import org.nuxeo.runtime.api.Framework;
 
 public class ArtifactSearcherImpl implements ArtifactSearcher {
 
-    protected static final int MAX_RESULTS = 1000;
-
     protected NuxeoArtifact mapDoc2Artifact(DocumentModel doc) {
         NuxeoArtifact artifact = null;
 
