cb07fd5967c516c0ea4911aca65355291d8ba665
Anahide Tchertchian <atchertchian@nuxeo.com>
2021-03-19 15:14:50 +0100

    NXP-30285: setup persistent preview on 10.10

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -37,8 +37,34 @@ String getVersion(referenceBranch) {
   return BRANCH_NAME == referenceBranch ? version : version + "-${BRANCH_NAME}"
 }
 
+void getNuxeoVersion() {
+  container('maven') {
+    return sh(returnStdout: true, script: 'mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=nuxeo.platform.version -q -DforceStdout').trim()
+  }
+}
+
 String getCommitSha1() {
   return sh(returnStdout: true, script: 'git rev-parse HEAD').trim();
+}
+
+void dockerPull(String image) {
+  sh "docker pull ${image}"
+}
+
+String getPreviewNamespace(isReferenceBranch, branchName) {
+  if (isReferenceBranch) {
+    return 'nuxeo-explorer-test'
+  }
+  return "nuxeo-explorer-${branchName.toLowerCase()}"
+}
+
+String getPreviewTemplatesOverride(isReferenceBranch) {
+  if (isReferenceBranch) {
+    // activate dedicated profiles on 21.0_11.3 preview
+    return 'nuxeo.templates=default,mongodb,explorer-export,explorer-stats'
+  }
+  // NXP-29494: override templates to avoid activating s3 in PR preview + add stats
+  return 'nuxeo.templates=default,explorer-stats'
 }
 
 pipeline {
@@ -52,9 +78,19 @@ pipeline {
     )
   }
   environment {
+    MAVEN_OPTS = "$MAVEN_OPTS -Xms512m -Xmx3072m"
+    MAVEN_ARGS = '-B -nsu'
     REFERENCE_BRANCH = "18.0_10.10"
     SCM_REF = "${getCommitSha1()}"
     VERSION = "${getVersion(REFERENCE_BRANCH)}"
+    PERSISTENCE = "${BRANCH_NAME == REFERENCE_BRANCH}"
+    NUXEO_TEMPLATE_OVERRIDE = "${getPreviewTemplatesOverride(BRANCH_NAME == REFERENCE_BRANCH)}"
+    NUXEO_IMAGE_VERSION = getNuxeoVersion()
+    NUXEO_DOCKER_REGISTRY = 'docker-private.packages.nuxeo.com'
+    PREVIEW_NAMESPACE = "${getPreviewNamespace(BRANCH_NAME == REFERENCE_BRANCH, BRANCH_NAME)}"
+    // APP_NAME and ORG needed for PR preview
+    APP_NAME = 'nuxeo-explorer-test'
+    ORG = 'nuxeo'
   }
   stages {
     stage('Set labels') {
@@ -72,30 +108,187 @@ pipeline {
         }
       }
     }
-    stage('Compile and test') {
+    stage('Compile') {
       steps {
-        setGitHubBuildStatus('explorer/compile', 'Compile and test', 'PENDING')
+        setGitHubBuildStatus('explorer/compile', 'Compile', 'PENDING')
         container('maven') {
           echo """
           ----------------------------------------
           Compile
           ----------------------------------------"""
           echo "MAVEN_OPTS=$MAVEN_OPTS"
-          // skip functional tests: no firefox on 10.10 pods
-          sh 'mvn -B -nsu -DskipDocker -DskipITs install'
+          sh "mvn ${MAVEN_ARGS} -DskipTests -DskipDocker install"
         }
-        // findText regexp: ".*ERROR.*", fileSet: "ftests/**/log/server.log", unstableIfFound: true
+      }
+      post {
+        always {
+          archiveArtifacts artifacts: '**/target/*.jar, **/target/*.war, **/target/nuxeo-*-package-*.zip'
+        }
+        success {
+          setGitHubBuildStatus('explorer/compile', 'Compile', 'SUCCESS')
+        }
+        unsuccessful {
+          setGitHubBuildStatus('explorer/compile', 'Compile', 'FAILURE')
+        }
+      }
+    }
+    stage('Run Unit Tests') {
+      steps {
+        setGitHubBuildStatus('explorer/utests', 'Run Unit Tests', 'PENDING')
+        container('maven') {
+          echo """
+          ----------------------------------------
+          Run Unit Tests
+          ----------------------------------------"""
+          echo "MAVEN_OPTS=$MAVEN_OPTS"
+          sh "mvn  ${MAVEN_ARGS} -f modules test"
+        }
       }
       post {
         always {
           archiveArtifacts artifacts: '**/target/*.jar, **/target/*.war, **/target/nuxeo-*-package-*.zip, **/target/**/*.log, **/target/*.png, **/target/*.html'
-          junit testResults: '**/target/surefire-reports/*.xml, **/target/failsafe-reports/*.xml', allowEmptyResults: true
+          junit testResults: '**/target/surefire-reports/*.xml', allowEmptyResults: true
         }
         success {
-          setGitHubBuildStatus('explorer/compile', 'Compile and test', 'SUCCESS')
+          setGitHubBuildStatus('explorer/utests', 'Run Unit Tests', 'SUCCESS')
         }
         unsuccessful {
-          setGitHubBuildStatus('explorer/compile', 'Compile and test', 'FAILURE')
+          setGitHubBuildStatus('explorer/utests', 'Run Unit Tests', 'FAILURE')
+        }
+      }
+    }
+    stage('Build Docker Images') {
+      when {
+        anyOf {
+          branch 'PR-*'
+          branch "${REFERENCE_BRANCH}"
+        }
+      }
+      steps {
+        setGitHubBuildStatus('explorer/docker/build', 'Build Docker Images', 'PENDING')
+        container('maven') {
+          echo """
+          ----------------------------------------
+          Build Explorer Docker image
+          ----------------------------------------
+          Image tag: ${VERSION}
+          Registry: ${DOCKER_REGISTRY}
+          """
+          script {
+            def moduleDir = 'docker/nuxeo-explorer-docker'
+            // push images to the Jenkins X internal Docker registry
+            sh "envsubst < ${moduleDir}/skaffold.yaml > ${moduleDir}/skaffold.yaml~gen"
+            retry(2) {
+              sh "skaffold build -f ${moduleDir}/skaffold.yaml~gen"
+            }
+          }
+        }
+      }
+      post {
+        success {
+          setGitHubBuildStatus('explorer/docker/build', 'Build Docker Images', 'SUCCESS')
+        }
+        unsuccessful {
+          setGitHubBuildStatus('explorer/docker/build', 'Build Docker Images', 'FAILURE')
+        }
+      }
+    }
+    stage('Deploy Preview') {
+      when {
+        anyOf {
+          branch "${REFERENCE_BRANCH}"
+          allOf {
+            branch 'PR-*'
+            expression {
+              return pullRequest.labels.contains('preview')
+            }
+          }
+        }
+      }
+      steps {
+        setGitHubBuildStatus('explorer/preview/deploy', 'Deploy Preview', 'PENDING')
+        container('maven') {
+          dir('helm/preview') {
+            echo """
+            ----------------------------------------
+            Deploy Preview environment
+            ----------------------------------------"""
+            script {
+              boolean isReferenceBranch = BRANCH_NAME == REFERENCE_BRANCH
+              // first substitute docker image names and versions
+              sh 'mv values.yaml values.yaml.tosubst'
+              if (isReferenceBranch) {
+                withCredentials([usernamePassword(credentialsId: 'explorer-preview-maintenance', passwordVariable: 'EXPLORER_PASSWORD', usernameVariable: 'UNUSED_USERNAME')]) {
+                  sh '''
+                    NUXEO_EXPLORER_CUSTOM_PARAMS=org.nuxeo.apidoc.apidocAdmin.password=$EXPLORER_PASSWORD \
+                      envsubst < values.yaml.tosubst > values.yaml
+                  '''
+                }
+              } else {
+                sh 'envsubst < values.yaml.tosubst > values.yaml'
+              }
+              // second create target namespace (if doesn't exist) and copy secrets to target namespace
+              String currentNs = sh(returnStdout: true, script: 'jx -b ns | sed -r "s/^Using namespace \'([^\']+)\'.+\\$/\\1/"').trim()
+              boolean nsExist = sh(returnStatus: true, script: "kubectl get namespace ${PREVIEW_NAMESPACE}") == 0
+              // Only used with jx preview on pr branches
+              String noCommentOpt = '';
+              if (nsExist) {
+                noCommentOpt = '--no-comment'
+                // Previous preview deployment needs to be scaled to 0 to be replaced correctly
+                sh "kubectl --namespace ${PREVIEW_NAMESPACE} scale deployment preview --replicas=0"
+              } else {
+                sh "kubectl create namespace ${PREVIEW_NAMESPACE}"
+              }
+              try {
+                sh "kubectl --namespace platform get secret kubernetes-docker-cfg -ojsonpath='{.data.\\.dockerconfigjson}' | base64 --decode > /tmp/config.json"
+                sh """kubectl create secret generic kubernetes-docker-cfg \
+                    --namespace=${PREVIEW_NAMESPACE} \
+                    --from-file=.dockerconfigjson=/tmp/config.json \
+                    --type=kubernetes.io/dockerconfigjson --dry-run -o yaml | kubectl apply -f -"""
+                String previewCommand = isReferenceBranch ?
+                  // To avoid jx gc cron job, reference branch previews are deployed by calling jx step helm install instead of jx preview
+                  "jx step helm install --namespace ${PREVIEW_NAMESPACE} --name ${PREVIEW_NAMESPACE} --verbose ."
+                  // When deploying a pr preview, we use jx preview which gc the merged pull requests
+                  : "jx preview --namespace ${PREVIEW_NAMESPACE} --verbose --source-url=https://github.com/nuxeo/nuxeo-explorer --preview-health-timeout 15m ${noCommentOpt}"
+
+                // third build and deploy the chart
+                // waiting for https://github.com/jenkins-x/jx/issues/5797 to be fixed in order to remove --source-url
+                sh """
+                  helm init --client-only --stable-repo-url=https://charts.helm.sh/stable
+                  helm repo add local-jenkins-x http://jenkins-x-chartmuseum:8080
+                  jx step helm build --verbose
+                  mkdir target && helm template . --output-dir target
+                  ${previewCommand}
+                """
+                if (isReferenceBranch) {
+                  // When not using jx preview, we need to expose the nuxeo url by hand
+                  url = sh(returnStdout: true, script: "kubectl get svc --namespace ${PREVIEW_NAMESPACE} preview -o go-template='{{index .metadata.annotations \"fabric8.io/exposeUrl\"}}'")
+                  echo """
+                    ----------------------------------------
+                    Preview available at: ${url}
+                    ----------------------------------------"""
+                }
+              } catch (err) {
+                echo "Error while deploying preview environment: ${err}"
+                if (!nsExist) {
+                  echo "Deleting namespace ${PREVIEW_NAMESPACE}"
+                  sh "kubectl delete namespace ${PREVIEW_NAMESPACE}"
+                }
+                throw err
+              }
+            }
+          }
+        }
+      }
+      post {
+        always {
+          archiveArtifacts allowEmptyArchive: true, artifacts: '**/requirements.lock, **/charts/*.tgz, **/target/**/*.yaml'
+        }
+        success {
+          setGitHubBuildStatus('explorer/preview/deploy', 'Deploy Preview', 'SUCCESS')
+        }
+        unsuccessful {
+          setGitHubBuildStatus('explorer/preview/deploy', 'Deploy Preview', 'FAILURE')
         }
       }
     }
---
+++ docker/nuxeo-explorer-docker/install-packages.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+set -e
+
+usage() {
+  exec >&2
+  echo "Usage:"
+  echo "  install-packages.sh [--clid <clid>] [--connect-url <connecturl>] [--offline] <packagelist>"
+  exit 2
+}
+
+echo '===================='
+echo '- Install packages -'
+echo '===================='
+
+if [[ -f $NUXEO_HOME/configured ]]; then
+  echo "Nuxeo is already configured."
+  echo "This script must be used in a Dockerfile to install packages at build time."
+  exit 2
+fi
+
+# default arguments for mp-install command
+mpInstallArgs="--accept yes --relax no"
+
+while [ $# -ne 0 ]; do
+  case $1 in
+    --clid) clid=$2; shift 2 ;;
+    --connect-url) connect_url=$2; shift 2 ;;
+    --offline) offline=true; shift 1 ;;
+    -*) echo "Unknown option: $1" >&2; usage ;;
+    *) packages=$@; break ;;
+  esac
+done
+
+if [ -z "$packages" ]; then echo "No packages to install" >&2; usage; fi
+
+echo
+if [ -n "$clid" ]; then
+  echo "Setting CLID"
+  # Replace -- by a carriage return
+  clid="${clid//--/\\n}"
+  mkdir -p $NUXEO_HOME/nxserver/data/
+  printf "%b\n" "$clid" >> $NUXEO_HOME/nxserver/data/instance.clid
+fi
+
+if [ -n "$connect_url" ]; then
+  echo "Setting Connect URL: $connect_url"
+  printf "org.nuxeo.connect.url=%b\n" "$connect_url" >> $NUXEO_HOME/bin/nuxeo.conf
+fi
+
+if [ -n "$offline" ]; then
+  # Prevent nuxeoctl from reaching Connect
+  noConnectProperty=org.nuxeo.connect.server.reachable=false
+  echo "Setting $noConnectProperty"
+  echo $noConnectProperty >> $NUXEO_HOME/bin/nuxeo.conf
+  mpInstallArgs="$mpInstallArgs --nodeps"
+fi
+
+echo
+NUXEO_CONF=$NUXEO_HOME/bin/nuxeo.conf $NUXEO_HOME/bin/nuxeoctl mp-install $mpInstallArgs $packages
+
+echo
+if [ -n "$clid" ]; then
+  echo "Unsetting CLID"
+  rm -rf $NUXEO_HOME/nxserver/data/
+fi
+
+if [ -n "$connect_url" ]; then
+  echo "Unsetting Connect URL"
+  sed -i "/org.nuxeo.connect.url=/d" $NUXEO_HOME/bin/nuxeo.conf
+fi
+
+if [ -n "$offline" ]; then
+  echo "Unsetting $noConnectProperty"
+  sed -i "/$noConnectProperty/d" $NUXEO_HOME/bin/nuxeo.conf
+fi
+
+# Clean up package installation directories
+backupDir=$NUXEO_HOME/packages/backup
+tmpDir=$NUXEO_HOME/packages/tmp
+# SUPINT-1863: don't break jsf-ui package installation on a hotfix distrib
+#echo "Clean up package installation directories: $backupDir, $tmpDir"
+echo "Clean up package installation directories: $tmpDir"
+#rm -rf $backupDir
+rm -rf $tmpDir
+echo
+
+# Set appropriate permissions on distribution directory
+# SUPINT-1863: moved to Dockerfile as it takes ages
+#chmod -R g+rwX $NUXEO_HOME
